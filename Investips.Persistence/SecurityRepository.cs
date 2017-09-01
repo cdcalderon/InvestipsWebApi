c062352e6cd79c701d586ddedbdce3e4d5437e37
Carlos Calderon <cdcalderon@gmail.com>
2017-09-01 00:45:39 -0500

    Move Persistence classes to Investips.Persistence

---
+++ Investips.Persistence/SecurityRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Investips.Core;
+using Investips.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Investips.Persistence
+{
+    public class SecurityRepository : ISecurityRepository
+    {
+        private readonly InvestipsDbContext context;
+
+        public SecurityRepository(InvestipsDbContext context)
+        {
+            this.context = context;
+        }
+        public void Add(Security security)
+        {
+            context.Securities.Add(security);
+        }
+
+        public async Task<Security> GetSecurity(int id)
+        {
+            return await context.Securities.SingleOrDefaultAsync(s => s.Id == id);
+        }
+    }
+}
---
+++ Investips.Persistence/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Investips.Core;
+
+namespace Investips.Persistence
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly InvestipsDbContext context;
+        public UnitOfWork(InvestipsDbContext context)
+        {
+            this.context = context;
+
+        }
+        public async Task CompleteAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+    }
+}
